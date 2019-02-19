c3814a2d86397c70314ed9e2aea889e47fabf2a6
camilleadam006 <cam.adam@outlook.fr>
2019-02-19 10:32:56 +0100

    premier commit, ajout class Player

---
+++ MagiWorld/src/fr/magiWorld/player/Player.java
@@ -0,0 +1,42 @@
+package fr.magiWorld.player;
+
+public class Player {
+
+    private int level = 0;
+    private int health = 0;
+    private int strength = 0;
+    private int agility = 0;
+    private int intelligence = 0;
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+}
